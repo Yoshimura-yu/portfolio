aa1dac3fea3b9b7e2e3fa21623e6fd9810ab19b9
Yoshimura-yu <yu_yu_m@icloud.com>
2026-09-09 16:39:21 +0900

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ window.addEventListener("scroll", function() {
 
     const scroll = window.scrollY;
 
-    image.style.right = (0 + scroll * 0.1) + "px";
+    image.style.right = (-200 + scroll * 0.1) + "px";
 
     image.style.opacity = Math.min(scroll / 500, 1);
 
