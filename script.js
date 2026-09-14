83ed33c6a93a9345a2814c3ea3ecccba9acbe6cc
Yoshimura-yu <yu_yu_m@icloud.com>
2026-09-14 14:34:00 +0900

    Update script.js

--- script.js
+++ script.js
@@ -13,3 +13,59 @@ window.addEventListener("scroll", function() {
     image.style.opacity = Math.min(scroll / 1200, 1); 
 
 });
+
+const tabs = document.querySelectorAll(".work-tab");
+const cards = document.querySelectorAll(".work-card");
+
+let currentIndex = 0;
+let timer;
+
+
+function showWork(index) {
+
+    tabs.forEach(tab => tab.classList.remove("active"));
+    cards.forEach(card => card.classList.remove("active"));
+
+    tabs[index].classList.add("active");
+    cards[index].classList.add("active");
+
+    currentIndex = index;
+}
+tabs.forEach((tab, index) => {
+
+    tab.addEventListener("mouseenter", () => {
+
+        clearInterval(timer);
+
+        showWork(index);
+    });
+
+});
+
+function startAutoSlide() {
+
+    timer = setInterval(() => {
+
+        currentIndex++;
+
+        if (currentIndex >= cards.length) {
+            currentIndex = 0;
+        }
+
+        showWork(currentIndex);
+
+    }, 5000);
+
+}
+tabs.forEach(tab => {
+
+    tab.addEventListener("mouseleave", () => {
+
+        startAutoSlide();
+
+    });
+
+});
+
+startAutoSlide();
+
